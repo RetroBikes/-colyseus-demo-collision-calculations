f1272c9425d47c5cc032168309188ecce563bac4
Victor Hugo <vct.bta@gmail.com>
2019-09-07 14:02:42 -0300

    Change home component from function to class

--- client-react/src/pages/home/Home.js
+++ client-react/src/pages/home/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Button, Heading } from 'arwes';
 import './home.css';
 
-const Home = () => (
-  <div class="home">
-    <div class="home-inner">
-      <Heading node='h1'>React Colyseus Demo</Heading>
-      <Button animate layer='alert'>Entrar</Button>
-    </div>
-  </div>
-);
+class Home extends React.Component {
+  render () {
+    return (
+      <div class="home">
+        <div class="home-inner">
+          <Heading node='h1'>React Colyseus Demo</Heading>
+          <Button animate layer='alert'>Entrar</Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;

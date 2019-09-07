76b1201cbb9efde39cfb73238d07cb84bef5e099
Victor Hugo <vct.bta@gmail.com>
2019-09-07 13:22:02 -0300

    Add home title and button

--- client-react/src/pages/Home.js
+++ client-react/src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Button, Heading } from 'arwes';
 
 const Home = () => (
-    <p>HOME</p>
+  <div class="home">
+    <Heading node='h1'>React Colyseus Demo</Heading>
+    <Button animate layer='alert'>Entrar</Button>
+  </div>
 );
 
 export default Home;

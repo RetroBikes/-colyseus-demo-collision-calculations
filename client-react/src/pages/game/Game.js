46dffaec8caa90a414046a968b40366ae8057660
Victor Hugo <vct.bta@gmail.com>
2019-09-07 18:01:29 -0300

    Copy game logic from static client to the react version

--- client-react/src/pages/game/Game.js
+++ client-react/src/pages/game/Game.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Frame } from 'arwes';
 import './game.css';
+import * as Colyseus from 'colyseus.js';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    const client = new Colyseus.Client('ws://localhost:2567');
+    client.joinOrCreate('my_room').then(room => this.initializeGame(room));
+  }
+
   render () {
     return (
       <div class="game">
@@ -10,6 +17,44 @@ class Game extends React.Component {
       </Frame>
     </div>
     );
+  }
+
+  initializeGame(room) {
+    var players = {};
+    var colors = ['red', 'green', 'yellow', 'blue', 'cyan', 'magenta'];
+
+    // listen to patches coming from the server
+    room.state.players.onAdd = function (player, sessionId) {
+      var dom = document.createElement("div");
+      dom.className = "player";
+      dom.style.left = player.x + "px";
+      dom.style.top = player.y + "px";
+      dom.style.background = colors[Math.floor(Math.random() * colors.length)];
+      dom.innerHTML = "Player " + sessionId;
+
+      players[sessionId] = dom;
+      document.body.appendChild(dom);
+    }
+
+    room.state.players.onRemove = function (player, sessionId) {
+      document.body.removeChild(players[sessionId]);
+      delete players[sessionId];
+    }
+
+    room.state.players.onChange = function (player, sessionId) {
+      var dom = players[sessionId];
+      dom.style.left = player.x + "px";
+      dom.style.top = player.y + "px";
+    }
+
+    window.addEventListener("keydown", function (e) {
+      switch (e.which) {
+        case 38: room.send({ position: 'up' }); break;
+        case 39: room.send({ position: 'right' }); break;
+        case 40: room.send({ position: 'down' }); break;
+        case 37: room.send({ position: 'left' }); break;
+      }
+    });
   }
 }
 

e0cdc235aa74badb1053833007fde425fd2810f6
Victor Hugo <vct.bta@gmail.com>
2019-09-07 17:50:55 -0300

    Convert function in class on Game component

--- client-react/src/pages/game/Game.js
+++ client-react/src/pages/game/Game.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Frame } from 'arwes';
 import './game.css';
 
-const Game = () => {
-
-  return (
-    <div class="game">
+class Game extends React.Component {
+  render () {
+    return (
+      <div class="game">
       <Frame animate={true} level={3} corners={4} layer='primary' classes="game-frame">
       </Frame>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Game;

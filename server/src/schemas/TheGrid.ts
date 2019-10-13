d09ea6fafd3f23fd5afd521f7266fd9c00cdd296
Victor Hugo <vct.bta@gmail.com>
2019-10-13 10:12:58 -0300

    Add isSpaceOccupied method on TheGrid class

--- server/src/schemas/TheGrid.ts
+++ server/src/schemas/TheGrid.ts
@@ -1,11 +1,26 @@
+import { Coordinate } from './Coordinate';
+
 export class TheGrid {
+
+    private gridSize: number;
+
     private gridItems: Array<Array<boolean>>;
 
     public constructor(gridSize: number) {
-        const emptyLine = new Array<boolean>((gridSize + 2));
+        this.gridSize = gridSize;
+        const emptyLine = new Array<boolean>((gridSize));
         emptyLine.fill(false);
-        this.gridItems = new Array<Array<boolean>>(gridSize + 2);
+        this.gridItems = new Array<Array<boolean>>(gridSize);
         this.gridItems.fill(emptyLine);
         console.log(this.gridItems);
     }
+
+    public isSpaceOccupied(spaceCoordinate: Coordinate): boolean {
+        if (0 > spaceCoordinate.x || this.gridSize < spaceCoordinate.x ||
+            0 > spaceCoordinate.y || this.gridSize < spaceCoordinate.y) {
+            return true;
+        }
+        return this.gridItems[spaceCoordinate.x][spaceCoordinate.y];
+    }
+
 }

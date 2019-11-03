94a9c9393692a38ade606aa685ae7701b81e9b9c
Victor Hugo <vct.bta@gmail.com>
2019-11-02 22:46:40 -0300

    Update the home title to demo IV

--- client-react/src/pages/home/Home.js
+++ client-react/src/pages/home/Home.js
@@ -26,8 +26,8 @@ class Home extends React.Component {
               </Layer>
             </Stage>
           </Frame>
-          <Heading node='h1'>React Colyseus Demo III</Heading>
-          <Heading node='h2'>The collision tests</Heading>
+          <Heading node='h1'>React Colyseus Demo IV</Heading>
+          <Heading node='h2'>More players</Heading>
           <Button animate layer='alert'>
             <Link href="/game">Start demo</Link>
           </Button>
